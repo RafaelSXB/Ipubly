dac421ff26aebcac9cde865345036dc0cb3f9ec1
RafaelSXB <rafael.bento1522@gmail.com>
2025-06-09 22:45:07 -0300

    Implementação do LoginController, validação do login e retorno do Token.

---
+++ src/main/java/com/project/Ipubly/Controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.project.Ipubly.Controller;
+
+import com.project.Ipubly.Model.DTO.LoginRequestDTO;
+import com.project.Ipubly.Model.DTO.LoginResponseDTO;
+import com.project.Ipubly.Services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/login/auth")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+        LoginResponseDTO responseDTO = loginService.loginAuth(loginRequest);
+       if(responseDTO.getUsername() == null) {
+           return ResponseEntity.badRequest().body(responseDTO);
+       }
+
+        return ResponseEntity.ok(responseDTO);
+    }
+}
--- src/main/java/com/project/Ipubly/Services/LoginService.java
+++ src/main/java/com/project/Ipubly/Services/LoginService.java
@@ -1,36 +1,49 @@
 package com.project.Ipubly.Services;
 
-import com.project.Ipubly.Model.DTO.UserRequestDTO;
-import com.project.Ipubly.Model.DTO.UserResponseDTO;
+import com.project.Ipubly.Model.DTO.LoginRequestDTO;
+import com.project.Ipubly.Model.DTO.LoginResponseDTO;
+import com.project.Ipubly.Model.UserEntity;
 import com.project.Ipubly.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LoginService {
-   /* @Autowired
+    @Autowired
     private JwtService jwtService;
 
     @Autowired
     private UsersRepository usersRepository;
 
-    public Login loginAuth(UserRequestDTO userRequestDTO) {
+    public LoginResponseDTO loginAuth(LoginRequestDTO loginRequestDTO) {
 
-       usersRepository.findByUsernameAndPassword(String userRequestDTO.getUsername(), String verifyPassword(userRequestDTO.getPassword()))
-                .orElseThrow(() -> new RuntimeException("Invalid username or password"));
-        UserResponseDTO userResponse = new UserResponseDTO();
-        userResponse.setUsername(userRequestDTO.getUsername());
-        userResponse.setToken("dummy-token");
-        userResponse.setMessage("Login successful");
+       UserEntity user = usersRepository.findByUsername(loginRequestDTO.getUsername()).orElseThrow(
+                () -> new IllegalArgumentException("User not found with username: " + loginRequestDTO.getUsername())
+        );
 
-        return userResponse;
+        if (!verifyPassword(loginRequestDTO.getPassword(), user.getPassword())){
+            return new LoginResponseDTO(loginRequestDTO.getUsername(), null, "Invalid Password");
+
+        };
+
+        String token = jwtService.generateToken(user.getId().toString(), user.getUsername());
+
+        LoginResponseDTO response = new LoginResponseDTO();
+        response.setUsername(user.getUsername());
+        response.setToken(token);
+
+        response.setMessage("Login successful");
+
+        return response;
 
     }
 
-    public verifyPassword(String password) {
-        // Implement your password verification logic here
-        // For example, you might want to hash the password and compare it with the stored hash
-        return password; // Placeholder, replace with actual verification logic
+    public Boolean verifyPassword(String password, String hashedPassword) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.matches(password, hashedPassword);
     }
-*/
+
 }
